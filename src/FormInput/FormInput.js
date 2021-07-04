451ca2bff97ef9e6936ff81fe202f606e2af7b84
Aleksandr Biryukov <missliss1@yandex.ru>
2021-07-04 16:09:40 +0300

    Refactoring Seacrh

--- src/FormInput/FormInput.js
+++ src/FormInput/FormInput.js
@@ -1,36 +1,41 @@
 import React from 'react';
-import {View, TextInput, StyleSheet} from 'react-native'
+import {View, TextInput, StyleSheet} from 'react-native';
 
-const FormInput = (props) => {
-	return (
-		<View>
-			<TextInput style={!props.theme ? stylesBlack.input : stylesWhite.input} placeholderTextColor={'#aaa'} 
-					onChangeText={props.searchName} onBlur={props.userSearch} 
-					value={props.name.name} placeholder ="Введите имя"/>
-		</View>
-		)
-}
+const FormInput = ({theme, searchName, userSearch, name}) => {
+  return (
+    <View>
+      <TextInput
+        style={!theme ? stylesBlack.input : stylesWhite.input}
+        placeholderTextColor={'#aaa'}
+        onChangeText={searchName}
+        onBlur={userSearch}
+        value={name.name}
+        placeholder="Введите имя"
+      />
+    </View>
+  );
+};
 
 const stylesBlack = StyleSheet.create({
-    input: {
-	backgroundColor: '#000',
+  input: {
+    backgroundColor: '#000',
     fontSize: 25,
     color: '#fff',
     paddingLeft: 10,
     borderColor: '#aaa',
-    borderBottomWidth: 1
-	}
-})
+    borderBottomWidth: 1,
+  },
+});
 
 const stylesWhite = StyleSheet.create({
-    input: {
-	backgroundColor: '#fff',
+  input: {
+    backgroundColor: '#fff',
     fontSize: 25,
     color: '#000',
     paddingLeft: 10,
     borderColor: '#aaa',
-    borderBottomWidth: 1
-	}
-})
+    borderBottomWidth: 1,
+  },
+});
 
-export default FormInput
+export default FormInput;
--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,119 +1,171 @@
-import React, {useState, useEffect, useDebugValue, Children} from 'react';
-import getQuery from './../../api.js'
-import { View, StyleSheet, ActivityIndicator, ScrollView, Image} from 'react-native';
-import {country_data} from './../redux/state'
-import UsersData from './../UsersData/UsersData'
-import FormInput from './../FormInput/FormInput'
-import Menu from './../Menu/Menu'
-import Data from './../Data/Data'
-import ThemeContainer from './../ThemeContainer/ThemeContainer'
+import React, {useState, useEffect} from 'react';
+import getQuery from './../../api.js';
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  ScrollView,
+  Image,
+} from 'react-native';
+import {country_data} from './../redux/state';
+import UsersData from './../UsersData/UsersData';
+import FormInput from './../FormInput/FormInput';
+import Menu from './../Menu/Menu';
+import Data from './../Data/Data';
+import ThemeContainer from './../ThemeContainer/ThemeContainer';
 import FavoritesMenu from '../FavoritesMenu/FavoritesMenu';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
-const Search = (props) => {
+const Search = ({
+  setName,
+  city_id,
+  token,
+  setUsers,
+  name,
+  setCountry,
+  setCountryId,
+  setCity,
+  theme,
+  country,
+  navigation,
+  users,
+}) => {
   let [load, setLoad] = useState(false);
   let [data, setData] = useState([]);
   let [hide, setHide] = useState(false);
   let [hideCity, setHideCity] = useState(false);
   let [cityName, setCityname] = useState('');
   let [id_c, setCid] = useState('');
-  let [id, setId] = useState('')
-  let [username, setUsername] = useState('')
-  let [menu, setMenu] = useState(false)
-	let showMenu = () => {
-		hide == false ? setHide(true) : setHide(false);
-    }
-  let searchName = (e) => {
-    props.setName(e);
-  }
-    useEffect(() => {
-			let method = 'users.search';
-      let params = `q=${props.name.name}&country=${id_c}&city=${props.city_id.city_id}&count=50&fields=photo_100,first_name,last_name,city,country,online`;
-      let token = props.token.token;
-			getQuery(method, params, token).then(data => {
-				props.setUsers(data.response.items);
-        setData(data.response.items);
-        setLoad(true);
-      })
-	}, [data.count])
+  let [id, setId] = useState('');
+  let [username, setUsername] = useState('');
+  let [menu, setMenu] = useState(false);
+  let showMenu = () => {
+    hide === false ? setHide(true) : setHide(false);
+  };
+  let searchName = e => {
+    setName(e);
+  };
+  useEffect(() => {
+    let method = 'users.search';
+    let params = `q=${name.name}&country=${id_c}&city=${
+      city_id.city_id
+    }&count=50&fields=photo_100,first_name,last_name,city,country,online`;
+    getQuery(method, params, token.token).then(data => {
+      setUsers(data.response.items);
+      setData(data.response.items);
+      setLoad(true);
+    });
+  }, [data.count]);
 
   let userSearch = () => {
     let method = 'users.search';
-    let params = `q=${props.name.name}&country=${id_c}&city=${props.city_id.city_id}&count=50&fields=photo_100,first_name,last_name,city,country,online`;
-    let token = props.token.token;
-		getQuery(method, params, token).then(data => {
-			props.setUsers(data.response.items);
+    let params = `q=${name.name}&country=${id_c}&city=${
+      city_id.city_id
+    }&count=50&fields=photo_100,first_name,last_name,city,country,online`;
+    getQuery(method, params, token.token).then(data => {
+      setUsers(data.response.items);
       setData(data.response.items);
       setLoad(true);
-		})
-  }
+    });
+  };
 
-
-  let getCountry = (e) => {
-    props.setCountry(e);
-  }
+  let getCountry = e => {
+    setCountry(e);
+  };
 
   let getCountryId = () => {
-    for (let i = 0; i < country_data.length; i++) {
-      if (props.country.country.toLowerCase() === country_data[i].title.toLowerCase()) {
-        props.setCountryId(country_data[i].id);
-        setCid(country_data[i].id);
+    country_data.forEach(item => {
+      if (country.country.toLowerCase() === item.title.toLowerCase()) {
+        setCountryId(item.id);
+        setCid(item.id);
       }
-    }
-  }
+    });
+  };
 
-  let getCity = (e) => {
-    props.setCity(e);
+  let getCity = e => {
+    setCity(e);
     setCityname(e);
     setHideCity(true);
-  }
+  };
 
-  const {navigate} = props.navigation;
+  const {navigate} = navigation;
 
   let user_data = (id, name) => {
-    props.setId(id);
+    setId(id);
     setUsername(name);
     setId(id);
     navigate('Пользователь', {user_id: id});
-  }
+  };
 
   const getMenu = () => {
-    menu ? setMenu(false) : setMenu(true)
-  }
+    menu ? setMenu(false) : setMenu(true);
+  };
 
   return (
-    <View style={!props.theme.theme ? stylesBlack.body : stylesWhite.body} >
+    <View style={!theme.theme ? stylesBlack.body : stylesWhite.body}>
       <View style={{position: 'relative'}}>
         <TouchableOpacity onPress={getMenu}>
-          <Image style={{width: 50, height: 50}} source={!props.theme.theme ? require('./reorder_white.png') : require('./reorder_black.png')} />
+          <Image
+            style={{width: 50, height: 50}}
+            source={
+              !theme.theme
+                ? require('./reorder_white.png')
+                : require('./reorder_black.png')
+            }
+          />
         </TouchableOpacity>
-        {menu && <FavoritesMenu navigate={navigate} theme={props.theme.theme} />}
+        {menu && <FavoritesMenu navigate={navigate} theme={theme.theme} />}
       </View>
       {<ThemeContainer />}
-      <FormInput {...props} userSearch={userSearch} searchName={searchName} theme={props.theme.theme}/>
-      <Menu cityName={cityName} showMenu={showMenu} 
-        hide={hide} getCountry={getCountry} 
-        getCountryId={getCountryId} 
-        getCity={getCity}
-        theme={props.theme.theme}/>
-      <Data userSearch={userSearch} setHideCity={setHideCity} 
-        hideCity={hideCity} setCityname={setCityname} 
+      <FormInput
+        userSearch={userSearch}
+        searchName={searchName}
+        theme={theme.theme}
+      />
+      <Menu
         cityName={cityName}
-        theme={props.theme.theme} {...props}/>
-      <ScrollView>{!load
-                      ? <View style={!props.theme.theme ? [stylesBlack.container, stylesBlack.horizontal] : [stylesWhite.container, stylesWhite.horizontal]}>
-                          <ActivityIndicator size="large" color="#0000ff" />
-                        </View>
-                      : props.users.users.map((item) => { 
-                          return <UsersData key={item.id} name = {item.first_name + " " + item.last_name}
-                                            user_data={user_data}
-                                            id = {item.id}
-                                            online = {item.online}
-                                            photo = {item.photo_100}
-                                            is_closed = {item.is_closed}
-                                            theme={props.theme.theme}
-                                            city = {'city' in item ? item.city.title : 'не указан'}/> 
-                        }) }
+        showMenu={showMenu}
+        hide={hide}
+        getCountry={getCountry}
+        getCountryId={getCountryId}
+        getCity={getCity}
+        theme={theme.theme}
+      />
+      <Data
+        userSearch={userSearch}
+        setHideCity={setHideCity}
+        hideCity={hideCity}
+        setCityname={setCityname}
+        cityName={cityName}
+        theme={theme.theme}
+      />
+      <ScrollView>
+        {!load ? (
+          <View
+            style={
+              !theme.theme
+                ? [stylesBlack.container, stylesBlack.horizontal]
+                : [stylesWhite.container, stylesWhite.horizontal]
+            }>
+            <ActivityIndicator size="large" color="#0000ff" />
+          </View>
+        ) : (
+          users.users.map(item => {
+            return (
+              <UsersData
+                key={item.id}
+                name={item.first_name + ' ' + item.last_name}
+                user_data={user_data}
+                id={item.id}
+                online={item.online}
+                photo={item.photo_100}
+                is_closed={item.is_closed}
+                theme={theme.theme}
+                city={'city' in item ? item.city.title : 'не указан'}
+              />
+            );
+          })
+        )}
       </ScrollView>
     </View>
   );
@@ -124,41 +176,41 @@ const stylesBlack = StyleSheet.create({
     alignContent: 'center',
     backgroundColor: 'black',
     width: '100%',
-    height: '100%'
+    height: '100%',
   },
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   horizontal: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-around',
     padding: 10,
-    alignItems: 'center'
-  }
-})
+    alignItems: 'center',
+  },
+});
 
 const stylesWhite = StyleSheet.create({
   body: {
     alignContent: 'center',
     backgroundColor: '#fff',
     width: '100%',
-    height: '100%'
+    height: '100%',
   },
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   horizontal: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-around',
     padding: 10,
-    alignItems: 'center'
-  }
-})
+    alignItems: 'center',
+  },
+});
 
 export default Search;
